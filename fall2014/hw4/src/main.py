161b7a568b4a0d4684847d014a5a7842d8ae2c7f
Nikita Kartashov <snailandmail@gmail.com>
2014-12-19 16:39:35 +0300

    Fixed image & point transformation

--- fall2014/hw4/src/main.py
+++ fall2014/hw4/src/main.py
@@ -1,12 +1,9 @@
 __author__ = 'nikita_kartashov'
 
-from math import sqrt
-
 import cv2
 import numpy as np
 
 from utils.cv_utils import read_image_grayscale, draw_matches
-from utils.py_utils import head
 
 
 IMAGE = '/Users/nikita_kartashov/Documents/Work/cv/fall2014/hw3/resource/mandril.bmp'
@@ -15,28 +12,15 @@ KEYPOINTS = '/Users/nikita_kartashov/Documents/Work/cv/fall2014/hw4/resource/key
 NEW_KEYPOINTS = '/Users/nikita_kartashov/Documents/Work/cv/fall2014/hw4/resource/new_keypoints.bmp'
 
 
-def rotate45(image):
+def transform_image(image):
     rows, cols = image.shape
-    rotation_matrix = cv2.getRotationMatrix2D((cols / 2, rows / 2), 45, 1)
+    rotation_matrix = cv2.getRotationMatrix2D((cols / 2, rows / 2), 45, 0.5)
     return cv2.warpAffine(image, rotation_matrix, image.shape)
 
 
-def resize_half(image):
-    return cv2.resize(image, (image.shape[0] / 2, image.shape[1] / 2))
-
-
-def manually_transform_keypoints(points, cols, rows):
-    rotation_matrix = np.empty((2, 2), np.float32)
-    rotation_matrix.fill(sqrt(2) / 2)
-    rotation_matrix[0, 1] *= -1
-    rotation_matrix[1, 0] *= 1
-
-    resize_matrix = np.zeros((2, 2), np.float32)
-
-    resize_matrix[0, 0] = 0.5
-    resize_matrix[1, 1] = 0.5
-    transformation_matrix = cv2.getRotationMatrix2D((cols / 2, rows / 2), 45, 0.5) # np.dot(resize_matrix, rotation_matrix)
-    return np.array([np.dot(cv2.transpose(transformation_matrix), point) for point in points])
+def transform_keypoints(points, cols, rows):
+    transformation_matrix = cv2.getRotationMatrix2D((cols / 2, rows / 2), 45, 0.5)
+    return np.array([np.dot(transformation_matrix, point) for point in points])
 
 
 NFEATURES = 1000
@@ -46,16 +30,14 @@ if __name__ == '__main__':
     image = read_image_grayscale(IMAGE)
     sift = cv2.SIFT(NFEATURES)
     initial_keypoints, initial_descriptors = sift.detectAndCompute(image, None)
-    old_points = np.array(map(lambda x: np.array(x.pt, np.float32), initial_keypoints))
-    transformed_points = manually_transform_keypoints(old_points, image.shape[0], image.shape[1])
-    transformed_points = [np.array([x, y])for x, y, _ in transformed_points]
+    old_points = np.array(map(lambda x: np.array([x.pt[0], x.pt[1], 1], np.float32), initial_keypoints))
+    transformed_points = transform_keypoints(old_points, image.shape[0], image.shape[1])
     keypoint_image = cv2.drawKeypoints(image, initial_keypoints, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-    rotated_image = rotate45(image)
-    resized_image = resize_half(rotated_image)
+    transformed_image = transform_image(image)
 
-    new_keypoints, new_descriptors = sift.detectAndCompute(resized_image, None)
-    new_keypoint_image = cv2.drawKeypoints(resized_image, new_keypoints,
+    new_keypoints, new_descriptors = sift.detectAndCompute(transformed_image, None)
+    new_keypoint_image = cv2.drawKeypoints(transformed_image, new_keypoints,
                                            flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     # FLANN parameters
@@ -69,13 +51,13 @@ if __name__ == '__main__':
 
     really_matched_points = []
     for i, (m, n) in enumerate(matches):
-        distance = np.linalg.norm(transformed_points[i] - new_keypoints[m.trainIdx].pt)
+        distance = np.linalg.norm(transformed_points[m.queryIdx] - new_keypoints[m.trainIdx].pt)
         really_matched_points.append(1 if distance < EPSILON else 0)
 
     fraction_matched = sum(really_matched_points) * 1.0 / (NFEATURES if NFEATURES else len(initial_keypoints))
     print('{0} fraction of points of interest were matched with epsilon = {1}'.format(fraction_matched, EPSILON))
 
-    result_image = draw_matches(image, initial_keypoints, resized_image, new_keypoints, matches)
+    result_image = draw_matches(image, initial_keypoints, transformed_image, new_keypoints, matches)
 
     cv2.imwrite(NEW_KEYPOINTS, new_keypoint_image)
     cv2.imwrite(KEYPOINTS, keypoint_image)
